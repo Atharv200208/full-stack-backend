405d2a81e360a763e159221aaf15d64dc80e9c1d
Atharv200208 <atharvr10@gmail.com>
2025-07-11 00:29:18 +0530

    subscription controller started 1 function done

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import asyncHandler from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { Subscription  } from "../models/subscription.model.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
+const toggleSubscription = asyncHandler(async(req, res) => {
+    const { channelId } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(channelId)) {
+        throw new ApiError(400, "Invalid video ID");
+    }
+
+    if(String(req.user._id) === channelId){
+        throw new ApiError(400,"You cannot subscribe to your ow channel")
+    }
+
+    const existingSubscription = await Subscription.findOne({
+        subscriber: req.user._id,
+        channel: channelId,
+    })
+
+    let message;
+    if(existingSubscription){
+        //Unsubcsribe
+        await Subscription.deleteOne();
+        message = "Unsubscribed from the channel"
+    }
+    else{
+        await Subscription.create({
+            subscriber: req.user._id,
+            channel: channelId
+        })
+        message = "Subscribed to the channel"
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            null,
+            message
+        )
+    )
+
+})
